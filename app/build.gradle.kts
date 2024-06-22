2f8eb24d90621345b6438125d4dbf3bea1e10a74
Lior Zalta <liorzalta24@gmail.com>
2024-06-22 03:05:04 +0300

    add podcasts functionlity in webview

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,4 +38,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("androidx.webkit:webkit:1.11.0")
 }
--- app/src/main/java/com/example/wait4it/Podcasts/UI/PodcastsMainPage.java
+++ app/src/main/java/com/example/wait4it/Podcasts/UI/PodcastsMainPage.java
@@ -1,4 +1,36 @@
 package com.example.wait4it.Podcasts.UI;
 
-public class PodcastsMainPage {
+import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.wait4it.R;
+
+public class PodcastsMainPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_podcasts_main_page);
+
+        WebView myWebView = (WebView) findViewById(R.id.podcasts_WEBVIEW_webView);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request){
+                // do your handling codes here, which url is the requested url
+                // probably you need to open that url rather than redirect:
+//                view.loadUrl(url);
+                return false; // then it is not handled by default action
+            }
+        });
+
+        myWebView.loadUrl("http://192.168.7.5:8000/podcasts");
+    }
 }
