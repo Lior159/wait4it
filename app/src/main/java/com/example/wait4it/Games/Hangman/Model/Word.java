c7fa9b39074ec879cd3695a3432e1cf688a2731c
MichaelSchv <47988141+MichaelSchv@users.noreply.github.com>
2024-06-30 07:07:10 +0300

    Images + Keyboard is working + UI is working, just need Data + Logic

---
+++ app/src/main/java/com/example/wait4it/Games/Hangman/Model/Word.java
@@ -0,0 +1,13 @@
+package com.example.wait4it.Games.Hangman.Model;
+
+public class Word {
+    private String word;
+
+    public Word(String word){
+        this.word = word;
+    }
+
+    public String getWord() {
+        return this.word;
+    }
+}
--- app/src/main/java/com/example/wait4it/Games/Hangman/UI/HangmanActivity.java
+++ app/src/main/java/com/example/wait4it/Games/Hangman/UI/HangmanActivity.java
@@ -7,8 +7,10 @@ import android.view.ViewGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.LinearLayoutCompat;
+import androidx.core.content.ContextCompat;
 
 import com.example.wait4it.R;
+import com.example.wait4it.Utilities.ImageLoader;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.imageview.ShapeableImageView;
 import com.google.android.material.textview.MaterialTextView;
@@ -16,11 +18,14 @@ import com.google.android.material.textview.MaterialTextView;
 public class HangmanActivity extends AppCompatActivity {
     private MaterialTextView hangman_LBL_title;
     private ShapeableImageView hangman_IMG_level;
-    private MaterialButton[] hangman_BTN_firstRowAtoI = new MaterialButton[9];
-    private MaterialButton[] hangman_BTN_secondRowJtoQ = new MaterialButton[8];
-    private MaterialButton[] hangman_BTN_thirdRowRtoZ = new MaterialButton[9];
-    private LinearLayoutCompat hangman_LBL_underscores;
+    private MaterialButton[] hangman_BTN_firstRowAtoG = new MaterialButton[7];
+    private MaterialButton[] hangman_BTN_secondRowHtoM = new MaterialButton[6];
+    private MaterialButton[] hangman_BTN_thirdRowNtoT = new MaterialButton[7];
+    private MaterialButton[] hangman_BTN_fourthRowUtoZ = new MaterialButton[6];
+    private LinearLayoutCompat hangman_LLC_underscores;
     private MaterialTextView[] letterViews;
+    ImageLoader imageLoader = new ImageLoader(this);
+    int status =0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +35,60 @@ public class HangmanActivity extends AppCompatActivity {
         String word = "Liverpool";
         String word2 = "Manchester United";
         setupWordViews(word2);
+        handleKeyboard();
+        loadImage();
 
     }
 
+    private void loadImage() {
+        String template = String.format("hangman_%d", status);
+        imageLoader.load(getResources().getIdentifier(template, "drawable", getPackageName()), R.drawable.ic_launcher_background, hangman_IMG_level);
+        status++;
+    }
+
+    private void handleKeyboard() {
+        for (int i = 'A'; i <= 'Z'; i++) {
+            int index = i-'A';
+            if(i>= 'A' && i<='G'){
+                hangman_BTN_firstRowAtoG[index].setOnClickListener(v->{
+                    hangman_BTN_firstRowAtoG[index].setOnClickListener(null);
+                    hangman_BTN_firstRowAtoG[index].setClickable(false);
+                    hangman_BTN_firstRowAtoG[index].setBackgroundTintList(ContextCompat.getColorStateList(this,R.color.blue_400));
+                    loadImage();
+                });
+            }
+            else if(i>='H' && i<='M')
+            {
+                hangman_BTN_secondRowHtoM[index-7].setOnClickListener(v->{
+                    hangman_BTN_secondRowHtoM[index-7].setOnClickListener(null);
+                    hangman_BTN_secondRowHtoM[index-7].setClickable(false);
+                    hangman_BTN_secondRowHtoM[index-7].setBackgroundTintList(ContextCompat.getColorStateList(this,R.color.blue_400));
+                });
+            }
+            else if(i>='N' && i<='T')
+            {
+                hangman_BTN_thirdRowNtoT[index-13].setOnClickListener(v->{
+                    hangman_BTN_thirdRowNtoT[index-13].setOnClickListener(null);
+                    hangman_BTN_thirdRowNtoT[index-13].setClickable(false);
+                    hangman_BTN_thirdRowNtoT[index-13].setBackgroundTintList(ContextCompat.getColorStateList(this,R.color.blue_400));
+                });
+            }
+            else
+            {
+                hangman_BTN_fourthRowUtoZ[index - 20].setOnClickListener(v->{
+                    hangman_BTN_fourthRowUtoZ[index - 20].setOnClickListener(null);
+                    hangman_BTN_fourthRowUtoZ[index - 20].setClickable(false);
+                    hangman_BTN_fourthRowUtoZ[index - 20].setBackgroundTintList(ContextCompat.getColorStateList(this,R.color.blue_400));
+                });
+            }
+            //20
+
+        }
+    }
+
     private void setupWordViews(String word) {
-        hangman_LBL_underscores.removeAllViews();
+        hangman_LLC_underscores.removeAllViews();
+        Log.d("Word", "Word = " + word);
         letterViews = new MaterialTextView[word.length()];
 
         for (int i = 0; i < word.length(); i++) {
@@ -47,36 +101,45 @@ public class HangmanActivity extends AppCompatActivity {
             }
             else
                 materialTextView.setText("    ");
-            materialTextView.setTextSize(24);
+            materialTextView.setTextSize(30);
             materialTextView.setGravity(Gravity.CENTER);
             materialTextView.setPadding(8,8,8,8);
             letterViews[i] = materialTextView;
-            hangman_LBL_underscores.addView(materialTextView);
+            hangman_LLC_underscores.addView(materialTextView);
         }
     }
 
     private void findViews() {
         hangman_LBL_title = findViewById(R.id.hangman_LBL_title);
         hangman_IMG_level = findViewById(R.id.hangman_IMG_level);
-        hangman_LBL_underscores = findViewById(R.id.hangman_LBL_underscores);
+        hangman_LLC_underscores = findViewById(R.id.hangman_LLC_underscores);
         for (int i = 'A'; i <= 'Z'; i++) {
             String template = String.format("hangman_BTN_%c", i);
             Log.d("Template", "Template = " + template);
             int index = i-'A';
-            if(i>= 'A' && i<='I'){
-                Log.d("FirstRow","i = " + i + ", index = " + index);
-                hangman_BTN_firstRowAtoI[index] = findViewById(getResources().getIdentifier(template, "id", getPackageName()));
+            if(i>= 'A' && i<='G'){
+                Log.d("First Row","i = " + i + ", (char)i = " + (char)i + ", index = " + index);
+                hangman_BTN_firstRowAtoG[index] = findViewById(getResources().getIdentifier(template, "id", getPackageName()));
+                //hangman_BTN_firstRowAtoI[index].setText(String.valueOf((char)i));
+                //Log.d("First id", "hangman_BTN_firstRowAtoI[" + index + "] = " + hangman_BTN_firstRowAtoI[index].getResources().getResourceEntryName(hangman_BTN_firstRowAtoI[index].getId()));
+                //Log.d("First text", "hangman_BTN_firstRowAtoI[" + index + "].getText = "+ hangman_BTN_firstRowAtoI[index].getText());
             }
-            else if(i>='J' && i<='Q')
+            else if(i>='H' && i<='M')
             {
-                Log.d("SecondRow","i = " + i + ", index = " + index);
-                hangman_BTN_secondRowJtoQ[index-9] = findViewById(getResources().getIdentifier(template, "id", getPackageName()));
+                //Log.d("Second Row","i = " + i +", (char)i = " + (char)i + ", index = " + (index-9));
+                hangman_BTN_secondRowHtoM[index-7] = findViewById(getResources().getIdentifier(template, "id", getPackageName()));
+            }
+            else if(i>='N' && i<='T')
+            {
+                //Log.d("Third Row","i = " + i +", (char)i = " + (char)i + ", index = " + (index-17));
+                hangman_BTN_thirdRowNtoT[index-13] = findViewById(getResources().getIdentifier(template, "id", getPackageName()));
             }
             else
             {
-                Log.d("SecondRow","i = " + i + ", index = " + index);
-                hangman_BTN_thirdRowRtoZ[index-17] = findViewById(getResources().getIdentifier(template, "id", getPackageName()));
+                hangman_BTN_fourthRowUtoZ[index - 20] = findViewById((getResources().getIdentifier(template,"id", getPackageName())));
             }
+            //20
+
         }
     }
 }
